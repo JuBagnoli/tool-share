8d9ecd3c80823e2c9718d0ae282a06455a6c34b1
JuBagnoli <julesbagnoli@gmail.com>
2020-03-20 18:38:21 +0000

    initial code

--- app.py
+++ app.py
@@ -6,3 +6,8 @@ app = Flask(__name__)
 @app.route('/home')
 def home():
     return "Share your tools here!!"
+
+from application import app
+
+if __name__=='__main__':
+    app.run(debug=True, host='0.0.0.0')
