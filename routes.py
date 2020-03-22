ae00bb230358d0f687848fd39295ffb99db403f7
Jules <61311580+JuBagnoli@users.noreply.github.com>
2020-03-22 19:10:37 +0000

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,111 +1,53 @@
 from application import app, db, bcrypt
 from flask import render_template, redirect, url_for, request
-from application.forms import PostForm, RegistrationForm, LoginForm
-from application.models import Posts, Users
-from flask_login import login_user, current_user, logout_user, login_required
+from application.forms import ToolForm, RegistrationForm
+from application.models import Tools, Users
 
 @app.route('/')
 @app.route('/home')
 def home():
-    postData = Posts.query.all()
-    return render_template('home.html', title='our project', blogData=postData)
+	postData = Tools.query.all()
+	return render_template('home.html', title='our project', toolData=toolData)
 
-@app.route('/register')
-def register():
- return render_template('register.html', title='Register here!')
-
-@app.route('/about')
-def about():
- return render_template('about.html', title='Why we do what we do')
-
-@app.route('/login')
-def login():
- return render_template('login.html', title='Log in here!')
-
-@app.route('/post', methods=['GET', 'POST'])
-def post():
-    form = PostForm()
-    if form.validate_on_submit():
-        postData = Posts(
-            first_name = form.first_name.data,
-            last_name = form.last_name.data,
-            title = form.title.data,
-            content = form.content.data
-        )
-
-        db.session.add(postData)
-        db.session.commit()
-
-        return redirect(url_for('home'))
-
-    else:
-        print(form.errors)
-
-    return render_template('post.html', title='Post', form=form)
-	postData = Posts.query.all()
-	return render_template('home.html', title='Home', posts=postData)
-
-@app.route('/about')
-def about():
-    return render_template('about.html', title='about')
 @app.route('/register', methods=['GET', 'POST'])
 def register():
 	if current_user.is_authenticated:
-		return redirect(url_for('home'))
+	return redirect(url_for('home'))
 	form = RegistrationForm()
 	if form.validate_on_submit():
 		hash_pw = bcrypt.generate_password_hash(form.password.data)
 		user = Users(
-			first_name=form.first_name.data,
-			last_name=form.last_name.data,
-			email=form.email.data,
-			password=hash_pw
-			)
+		first_name=form.first_name.data,
+		last_name=form.last_name.data,
+		email=form.email.data,
+		password=hash_pw
+		)
 
-		db.session.add(user)
-		db.session.commit()
-
-		return redirect(url_for('post'))
-
+	db.session.add(user)
+	db.session.commit()
+	return redirect(url_for('post'))
 	return render_template('register.html', title='Register', form=form)
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-	if current_user.is_authenticated:
-		return redirect(url_for('home'))
-	form = LoginForm()
-	if form.validate_on_submit():
-		user = Users.query.filter_by(email=form.email.data).first()
-		if user and bcrypt.check_password_hash(user.password, form.password.data):
-			login_user(user, remember=form.remember.data)
-			next_page = request.args.get('next')
-			if next_page:
-				return redirect(next_page)
-			else:
-				return redirect('home')
-	return render_template('login.html', title='Login', form=form)
+@app.route('/about')
+	def about():
+	return render_template('about.html', title='Why we do what we do')
 
-@app.route('/post', methods=['GET', 'POST'])
-@login_required
-def post():
-	form = PostForm()
+@app.route('/tools', methods=['GET', 'POST'])
+def tools():
+	form = ToolForm()
 	if form.validate_on_submit():
-		postData = Posts(
-			title=form.title.data,
-			content=form.content.data,
-			author=current_user
-		)
-		db.session.add(postData)
-		db.session.commit()
-		return redirect(url_for('home'))
-
+		toolData = Tools(
+		first_name = form.first_name.data,
+            	last_name = form.last_name.data,
+            	title = form.title.data,
+            	content = form.content.data
+        	)
+        db.session.add(toolData)
+        db.session.commit()
+        return redirect(url_for('home'))
+	
 	else:
-		print(form.errors)
-	return render_template('post.html', title='Post', form=form)
+        print(form.errors)
 
-@app.route('/logout')
-@login_required
-def logout():
-	logout_user()
-	return redirect(url_for('login'))
-© 2020 GitHub, Inc.
+	toolData = Tools.query.all()
+	return render_template('tool.html', title='Tools', form=form)
